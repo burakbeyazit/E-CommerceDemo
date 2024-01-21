9dcf955aedb2d3201247228c2b2d835cf3ac9d0e
Burak Beyazıt <85076862+burakbeyazit@users.noreply.github.com>
2024-01-21 14:34:16 +0300

    First Injection

---
+++ E-Com DotNet Project/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace E_Com_DotNet_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
